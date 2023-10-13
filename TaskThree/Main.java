ba14979e56f58006b9405116aec255cd1c89a630
moldamolda <118838437+moldamolda@users.noreply.github.com>
2023-10-13 12:13:10 +0200

    update forifloopinmain

--- TaskThree/Main.java
+++ TaskThree/Main.java
@@ -5,25 +5,29 @@ public class Main {
     public static void main(String[] args) {
 
         ArrayList<String> student1Courses = new ArrayList<>();
-        student1Courses.add("math");
-        student1Courses.add("math");
-        student1Courses.add("math");
+        student1Courses.add("Math");
+        student1Courses.add("English");
+        student1Courses.add("Geography");
 
         Student s1 = new Student("lars", student1Courses);
 
         ArrayList<String> teacher1Courses = new ArrayList<>();
-        student1Courses.add("math");
-        student1Courses.add("math");
-        student1Courses.add("math");
+        student1Courses.add("Math");
+        student1Courses.add("Chemistry");
+        student1Courses.add("Computer Science");
 
-        Teacher t1 = new Teacher("tove", teacher1Courses);
+        Teacher t1 = new Teacher("Tove", teacher1Courses);
 
         ArrayList<Person> persons = new ArrayList<>();
         persons.add(s1);
         persons.add(t1);
 
-        for (int i = 0; i < persons.size(); i++) {
-            persons.get(i).get("java 1.0");
+        for (Person person : persons) {
+           if(!person.addCourse("java 1.0")){
+               System.out.println("har allerede bestået");
+           }else{
+               System.out.println("kan ikke undervise i fag");
+           }
 
 
         }
